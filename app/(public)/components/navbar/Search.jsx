9c58e7ee1830de5513fa49ae4d05cbb31ad49847
Muhammad Nasir <mn660875@gmail.com>
2025-08-21 09:55:20 +0500

    update search Bar with Escape Key

--- app/(public)/components/navbar/Search.jsx
+++ app/(public)/components/navbar/Search.jsx
@@ -6,29 +6,37 @@ import toast from "react-hot-toast";
 
 export default function Search() {
   const [open, setOpen] = useState(false);
-  const [searchItem, setSearch ]= useState("")
+  const [searchItem, setSearch] = useState("");
 
-  // Disable body scroll when overlay is open
+  // Disable body scroll & handle Escape key
   useEffect(() => {
     if (open) {
       document.body.style.overflow = "hidden";
+
+      const handleEsc = (e, KeyboardEvent) => {
+        if (e.key === "Escape") {
+          setOpen(false);
+        }
+      };
+
+      window.addEventListener("keydown", handleEsc);
+      return () => {
+        window.removeEventListener("keydown", handleEsc);
+        document.body.style.overflow = "auto";
+      };
     } else {
       document.body.style.overflow = "auto";
     }
-    return () => {
-      document.body.style.overflow = "auto";
-    };
   }, [open]);
 
-  const handleSearch= ()=>{
-    if(!searchItem){
-      toast.error("Empty Search Bar")
+  const handleSearch = () => {
+    if (!searchItem) {
+      toast.error("Empty Search Bar");
     }
-  }
+  };
 
   return (
     <>
-   
       <div className="p-4">
         <button
           onClick={() => setOpen(true)}
@@ -38,7 +46,6 @@ export default function Search() {
         </button>
       </div>
 
-    
       <AnimatePresence>
         {open && (
           <motion.div
@@ -48,16 +55,15 @@ export default function Search() {
             transition={{ duration: 0.3 }}
             className="fixed inset-0 bg-white flex items-center justify-center z-50 overflow-hidden"
           >
-         
+           
             <button
               onClick={() => setOpen(false)}
               className="absolute right-6 top-6 p-2 rounded-full hover:bg-gray-200"
             >
               <X size={28} />
             </button>
-            
 
-            
+           
             <motion.div
               initial={{ y: -50, opacity: 0 }}
               animate={{ y: 0, opacity: 1 }}
@@ -65,20 +71,24 @@ export default function Search() {
               transition={{ duration: 0.3 }}
               className="w-full max-w-md px-6"
             >
-              <h1 className="text-center text-3xl font-bold mb-4">Press Esc To Close</h1>
+              <h1 className="text-center text-3xl font-bold mb-4">
+                Press Esc To Close
+              </h1>
               <div className="flex items-center gap-2 ">
-              <input
-                type="text"
-                placeholder="Search..."
-                className="w-full px-4 py-3 text-lg rounded-full   shadow-md border border-gray-300 focus:outline-none focus:ring-2 focus:ring-[#FE4F70] to-[#FFA387]"
-                autoFocus
-                value={searchItem}
-                onChange={(e)=>setSearch(e.target.value)}
-                
-              />
-              <button onClick={()=>handleSearch()} className='w-fit bg-gradient-to-r from-[#FE4F70] to-[#FFA387] text-white rounded-full text-lg px-4 py-3 cursor-pointer'>
-            Search
-          </button>
+                <input
+                  type="text"
+                  placeholder="Search..."
+                  className="w-full px-4 py-3 text-lg rounded-full shadow-md border border-gray-300 focus:outline-none focus:ring-2 focus:ring-[#FE4F70] to-[#FFA387]"
+                  autoFocus
+                  value={searchItem}
+                  onChange={(e) => setSearch(e.target.value)}
+                />
+                <button
+                  onClick={() => handleSearch()}
+                  className="w-fit bg-gradient-to-r from-[#FE4F70] to-[#FFA387] text-white rounded-full text-lg px-4 py-3 cursor-pointer"
+                >
+                  Search
+                </button>
               </div>
             </motion.div>
           </motion.div>
