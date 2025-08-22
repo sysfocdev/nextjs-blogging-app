5e17321fdf66b3406426e60fb2ac9fb5e83b8820
Muhammad Nasir <mn660875@gmail.com>
2025-08-22 13:37:29 +0500

    Admin Panal design

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,1 +1,34 @@
-import type { Metadata } from "next"; import { Montserrat } from "next/font/google"; import "./globals.css"; import {ThemeProvider} from '@/app/(public)/components/ThemeProvider'; import Header from "@/app/(public)/components/Header"; import Footer from "@/app/(public)/components/Footer"; import { Toaster } from "react-hot-toast"; const montserrat = Montserrat({ variable: "--font-montserrat", weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"], subsets: ["latin"], }); export const metadata: Metadata = { title: "Next Blogging Application", description: "Getting Started ...", }; export default function RootLayout({ children, }: Readonly<{ children: React.ReactNode; }>) { return ( <html lang='en' suppressHydrationWarning> <body className={${montserrat.className} antialiased max-w-[1680px] mx-auto}> <ThemeProvider attribute="class" defaultTheme="system" enableSystem disableTransitionOnChange > <Header /> {children} <Footer /> <Toaster reverseOrder={false} toastOptions={{ duration:2000, }}/> </ThemeProvider> </body> </html> ); }
+
+// app/layout.jsx
+import { Montserrat } from "next/font/google";
+import "./globals.css";
+import { ThemeProvider } from "@/app/(public)/components/ThemeProvider";
+import Header from "@/app/(public)/components/Header";
+import Footer from "@/app/(public)/components/Footer";
+import { Toaster } from "react-hot-toast";
+
+const montserrat = Montserrat({
+  variable: "--font-montserrat",
+  weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
+  subsets: ["latin"],
+});
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en" suppressHydrationWarning>
+      <body className={`${montserrat.className} antialiased max-w-[1680px] mx-auto`}>
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="system"
+          enableSystem
+          disableTransitionOnChange
+        >
+          <Header />
+          {children}
+          <Footer />
+          <Toaster reverseOrder={false} toastOptions={{ duration: 2000 }} />
+        </ThemeProvider>
+      </body>
+    </html>
+  );
+}
