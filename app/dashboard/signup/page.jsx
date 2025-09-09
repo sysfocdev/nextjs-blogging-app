8cd5c45cf71435f658a2771ac61c25e93daf9e88
Muhammad Nasir <mn660875@gmail.com>
2025-09-09 16:30:18 +0500

    Remove LocalHost

--- app/dashboard/signup/page.jsx
+++ app/dashboard/signup/page.jsx
@@ -24,7 +24,7 @@ export default function Page() {
     }
 
     try {
-      const res = await fetch("http://localhost:3000/api/users", {
+      const res = await fetch("/api/users", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
