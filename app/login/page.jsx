4e82b60503be035d805163ad0eeaf891cf729650
Muhammad Nasir <mn660875@gmail.com>
2025-09-16 11:49:59 +0500

    Updated Login Form with JWT

--- app/login/page.jsx
+++ app/login/page.jsx
@@ -30,7 +30,7 @@ export default function Page() {
 
     if (data.success) {
       // ✅ Cookie set karo
-      setCookie("auth", JSON.stringify(data.user), {
+      setCookie("auth_token", JSON.stringify(data.user), {
         maxAge: 60 * 60 * 24, // 1 din
       });
       localStorage.setItem("user", JSON.stringify(data.user)); 
